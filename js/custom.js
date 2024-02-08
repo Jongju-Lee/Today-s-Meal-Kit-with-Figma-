51faa8bb752c3592ec81bf328ffa28fd22a26f5f
jongju-Lee <jeje7@naver.com>
2024-02-09 00:04:49 +0900

    main page building

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,10 @@
 $(function () {
-  // Include html Files
-  // gnb
+  /* ##### Include html Files ##### */
+  /* gnb */
   $(".gnb_include").load("/include/gnb.html");
-  // main_header
+  /* main_header */
   $(".main_header_include").load("/include/main_header.html");
-  // Category - Accordion
+  /* ##### Category - Accordion ##### */
   $(".category_accordion .detail").eq(0).show();
   $(".category_accordion .title").click(function () {
     // click된것 제외한 나머지 모두 접기
@@ -16,17 +16,40 @@ $(function () {
     // click된것 active 클래스 추가
     $(this).addClass("active");
   });
-  // GNB - Focus Transform Color
+  /* ##### GNB - Focus Transform Color ##### */
   $(".gnb a").click(function () {
     $(this).toggleClass("active");
   });
-  // Button - All Clear
+  /* Clear Button - Search (삭제버튼) */
   $(".search_recent .btn_all_clear").click(function () {
-    // 전체삭제 버튼
     $(".search .search_recent .item").hide();
   });
-  // 삭제 버튼 (X버튼)
   $(".search_recent .btn_clear").click(function () {
     $(this).parent().slideUp();
+  });
+  /* ##### Clear Button - Cart (삭제버튼) ##### */
+  $(".cart .btn_all_clear").click(function () {
+    $(".cart_items").slideUp();
+  });
+  $(".cart .btn_clear").click(function () {
+    $(this).parent().parent().slideUp();
+  });
+  /* ##### Slick Slider - Main ##### */
+  /* front_slider */
+  $(".front_slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
+  /* goods_suggestion Slider */
+  $(".goods_suggestion_items").slick({
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    arrows: false,
+    dots: false,
+    autoplay: false,
   });
 });
